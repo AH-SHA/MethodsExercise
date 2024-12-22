b49d5f804b12836330a0bfb543b3ad9b11b8c4e1
Shannon Ashford <email.sa.coder@gmail.com>
2024-12-22 13:12:42 -0500

    Completed Methods Exercise Parts 1 and 2

---
+++ Math Operators - Methods - Part 2/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+    public class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine(Add(10, 2)); //  Output 12
+            Console.WriteLine(Multiply(10, 2)); // Output 20
+            Console.WriteLine(Subtract(10, 2)); // Output  8
+            Console.WriteLine(Divide(10, 2)); // Output 5
+
+
+
+            static int Add(int a, int b)
+            {
+                return a + b;
+
+            }
+            static int Multiply(int a, int b)
+            {
+                return 10 * 2;
+            }
+            static int Subtract(int a, int b)
+            {
+                return 10 - 2;
+            }
+            static float Divide(int a, int b)
+
+            {
+                return 10 / 2;
+            }
+
+        }
+    }
+
--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // prompt user to enter several elements of information
+
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();   //user input
+
+            Console.WriteLine("What is your favorite color?");
+            string userColor = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal?");
+            string userAnimal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite band?");
+            string userBand = Console.ReadLine();
+
+            Console.WriteLine($"My name is {userName}.The color of my favorite shirt is {userColor}. I like {userAnimal}. {userBand} is my favorite band.");
+
         }
     }
 }
